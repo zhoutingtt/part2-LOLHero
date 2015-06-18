3ca271ccb9a91811c4e0b29cbbba67d9faa50a63
jamesGu <790032475@qq.com>
2015-06-18 13:34:13 +0800

    加的注释

--- part2-LOLHero/ViewController.m
+++ part2-LOLHero/ViewController.m
@@ -34,9 +34,6 @@
     return _heros;
 }
 
-
-
-
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
@@ -48,16 +45,43 @@
 }
 
 #pragma mark - UITableViewDataSource
+/**
+ *  可实现，可不实现，定义有几组，默认返回1（一组），只在tableview的group样式下有效。
+ *
+ *  @param tableView 当前tableview
+ *
+ *  @return 有几组
+ */
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 2;
+}
+
+/**
+ *  必须实现的数据源方法，定义每个section有几行
+ *
+ *  @param tableView 当前tableview
+ *  @param section   第几组
+ *
+ *  @return 每个section有几行
+ */
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     return self.heros.count;
 }
 
-- (NSInteger)tableView:(UITableView *)tableView sectionForSectionIndexTitle:(NSString *)title atIndex:(NSInteger)index
-{
+- (NSInteger)tableView:(UITableView *)tableView sectionForSectionIndexTitle:(NSString *)title atIndex:(NSInteger)index{
+    
     return -1;
 }
 
+/**
+ *  必须实现的数据源方法，返回每个cell的样式，类型为UITableViewCell，它继承于view
+ *
+ *  @param tableView 当前tableview
+ *  @param indexPath 索引 indexpath.section(第几组) indexpath.row(第几行)
+ *
+ *  @return UITableViewCell
+ */
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     static NSString *identifier = @"heroCell";
@@ -72,13 +96,30 @@
     cell.imageView.image = [UIImage imageNamed:hero.icon];
     
     return  cell;
-    
 }
 
 #pragma mark - UITableViewDelegate
+/**
+ *  行高
+ *
+ *  @param tableView 当前tableview
+ *  @param indexPath 索引 indexpath.section(第几组) indexpath.row(第几行)
+ *
+ *  @return 高度
+ */
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     return 44;
 }
 
+/**
+ *  选中行的索引，用来触发选中时的动作
+ *
+ *  @param tableView 同上
+ *  @param indexPath 同上
+ */
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    UIAlertView *alert = [[UIAlertView alloc]initWithTitle:nil message:[NSString stringWithFormat:@"%ld,%ld",(long)indexPath.section,(long)indexPath.row] delegate:self cancelButtonTitle:@"确定" otherButtonTitles:nil, nil];
+    [alert show];
+}
 @end
